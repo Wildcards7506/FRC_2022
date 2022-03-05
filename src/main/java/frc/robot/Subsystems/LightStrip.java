1124c4405b6ad451f27c002a94e557f0e23ba54b
Wildcards <wildcards7506@gmail.com>
2022-03-05 14:11:02 -0600

    fixed

--- src/main/java/frc/robot/Subsystems/LightStrip.java
+++ src/main/java/frc/robot/Subsystems/LightStrip.java
@@ -1,16 +1,13 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Commands.LightStripCom;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class LightStrip extends SubsystemBase {
-  /** Creates a new LightStrip. */
+  // Creates a new LightStrip.
   private AddressableLED led;
   public AddressableLEDBuffer ledBuffer;
 
@@ -31,6 +28,6 @@ public class LightStrip extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    setDefaultCommand(new LightStripCom());
   }
 }
