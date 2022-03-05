03c78fd98c777d0d7d64d9b1d984a01ac5869d70
Driverstation <wildcards7506@gmail.com>
2022-03-05 15:00:17 -0600

    start to new logic

--- src/main/java/frc/robot/Subsystems/LightStrip.java
+++ src/main/java/frc/robot/Subsystems/LightStrip.java
@@ -4,12 +4,12 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Commands.LightStripCom;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Timer;
 
 public class LightStrip extends SubsystemBase {
   // Creates a new LightStrip.
   private AddressableLED led;
-  public AddressableLEDBuffer ledBuffer;
+  private AddressableLEDBuffer ledBuffer;
 
   public LightStrip(int port, int bufferSize) {
     led = new AddressableLED(port);
@@ -20,11 +20,18 @@ public class LightStrip extends SubsystemBase {
   }
 
   public void preMatch(){
-    for (var i = 0; i < ledBuffer.getLength(); i++){
-      ledBuffer.setHSV(i, 100, 100, 100);
-      SmartDashboard.putString("LightMode", "RED");
+    boolean set = false;
+    for (int i = 1; i <= ledBuffer.getLength(); i++){
+        if (i == 1){
+          ledBuffer.setHSV(0, 100, 100, 100);
+        } else if (ledBuffer.getLED(i - 1).red != 0 && !set){
+          ledBuffer.setHSV(i - 1, 100, 100, 100);
+          set = true;
+        } else {
+          ledBuffer.setHSV(i - 1, 0, 0, 0);
+        }
     }
-
+    Timer.delay(.1);
     led.setData(ledBuffer);
   }
 
